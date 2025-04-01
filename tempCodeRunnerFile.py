9cfbfd294f53489d5772f46589c6d5aa5eca529e
Da Teng <dateng2016@gmail.com>
2025-04-01 16:49:38 -0400

    a

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+    # try:
+    #     test_url = "https://warhistoryonline.com"  # Replace with your target website
+        
+    #     check_cookie_options(test_url)
+    # except:
+    #     pass
